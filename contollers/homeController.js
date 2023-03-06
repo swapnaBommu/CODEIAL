6db3cad0a9e5337040f686aa5ce0de14b60aca24
swapnaBommu <swapnabommu8143@gmail.com>
2023-03-06 23:11:09 +0530

    Display posts and related user

--- contollers/homeController.js
+++ contollers/homeController.js
@@ -1,9 +1,25 @@
+const Posts = require('../models/post');
+
 module.exports.home = function(req,res){
 
-    return res.render('home',{
-        title:"Home"
-    });
+    // Posts.find({}).then(posts =>{
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts : posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Posts.find({}).populate('user').then(posts =>{
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts : posts
+        });
+    })
+
+
 };
+
 
 module.exports.home2 = function(req,res){
     return res.send('<P>This is second home controlser</p>');
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const homeController = require('../contollers/homeController');
 
 router.get('/',homeController.home);
-router.get('/home2',homeController.home2);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 
