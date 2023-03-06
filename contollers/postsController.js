2a2f4f125171afee721464563da365979c3053e6
swapnaBommu <swapnabommu8143@gmail.com>
2023-03-06 22:34:18 +0530

    saving posts in the DB

--- contollers/postsController.js
+++ contollers/postsController.js
@@ -4,10 +4,12 @@ module.exports.create = function(req,res){
     Post.create({
         content:req.body.content,
         user:req.user._id
-    }, function(err,post){
-        if(err){console.log('error in creating the post'); return;}
-
+    }).then(post =>{
         return res.redirect('back');
-    })
+    }) 
+    .catch(err =>{
+        console.log('error in creating the post'); return
+        
+    });
     
 };
