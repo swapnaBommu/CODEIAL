f35bb811d48036489efeec815546d4ce4eb48416
swapnaBommu <swapnabommu8143@gmail.com>
2023-03-08 22:45:58 +0530

    nested population and display comment and related user

--- contollers/commentsController.js
+++ contollers/commentsController.js
@@ -2,21 +2,24 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 module.exports.create = function(req,res){
-    Post.findById(req.body.post).then(post =>{
-        Comment.create({
-            content:req.body.content,
-            post:req.body.post,
-            user:req.user._id
-        }).then(comment =>{
-            post.comments.push(comment);
-            post.save();
-            return res.redirect('/');
-        }).catch(err =>{
-            console.log("Error while updating the comment in db");
-        }).catch(err => {
-            console.log('Error while creating the comment');
-        })
-    })
+    Post.findById(req.body.post._id) 
+    .then( post =>{
+        if (post){
+            Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id})
+                .then(comment =>{
+                    post.comments.push(comment);
+                    post.save();
+                    res.redirect('/');
+                })
+                .catch(err =>{
+                    console.log('error in updating the comment in db: ',err);
+                })
+        }     
+
+    });
 
 
 };
--- contollers/homeController.js
+++ contollers/homeController.js
@@ -1,6 +1,8 @@
-const Posts = require('../models/post');
+const Post = require('../models/post');
 
 module.exports.home = function(req,res){
+     // console.log(req.cookies);
+    // res.cookie('user_id', 25);   
 
     // Posts.find({}).then(posts =>{
     //     return res.render('home',{
@@ -10,7 +12,15 @@ module.exports.home = function(req,res){
     // });
 
     //populate the user of each post
-    Posts.find({}).populate('user').then(posts =>{
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .then(posts =>{
         return res.render('home',{
             title:"Codeial | Home",
             posts : posts
