e8f14109c5f79493d5fafb579ba5a462a5cb8cf1
swapnaBommu <swapnabommu8143@gmail.com>
2023-03-11 00:55:43 +0530

    update profile details

--- contollers/usersController.js
+++ contollers/usersController.js
@@ -2,13 +2,27 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title:"Users"
-    });
+    User.findById(req.params.id).then(user =>{
+        return res.render('user_profile',{
+            title:"Users",
+            profile_user:user
+        });
+    })
+    
 };
-module.exports.name = function(req,res){
-    return res.end("<h1>USERS name</h1>");
-};
+
+//update user information
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body).then(user =>{
+            return res.redirect('back');
+        })
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
+
 
 //render the sign up page
 module.exports.signUp = function(req,res){
